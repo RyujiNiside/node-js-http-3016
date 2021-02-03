8e1263bd4b1cfcdf8e7841e7c6abfaf63730fe21
あなたの名前 <nsd780384@gmail.com>
2021-02-04 01:09:36 +0900

    Herokuで実行させるための対応

--- index.js
+++ index.js
@@ -1,53 +1,25 @@
 'use strict';
 const http = require('http');
 const pug = require('pug');
-const server = http.createServer((req, res) => {
-  const now = new Date();
-  console.info('[' + now + '] Requested by ' + req.connection.remoteAddress);
-  res.writeHead(200, {
-    'Content-Type': 'text/html; charset=utf-8'
+'use strict';
+const http = require('http');
+const auth = require('http-auth');
+const router = require('./lib/router');
+
+const basic = auth.basic({
+    realm: 'Enter username and password.',
+    file: './users.htpasswd'
   });
 
-  switch (req.method) {
-    case 'GET':
-      if (req.url === '/enquetes/yaki-shabu') {
-        res.write(pug.renderFile('./posts.pug', {
-          path: req.url,
-        }));
-      } else if (req.url === '/posts') {
-        res.write(pug.renderFile('./posts.pug', {
-          path: req.url,
-        }));
-      } else if (req.url === '/enquetes/sushi-pizza') {
-        res.write(pug.renderFile('./form.pug', {
-          path: req.url,
-          firstItem: '寿司',
-          secondItem: 'ピザ'
-        }));
-      }
-      res.end();
-      break;
-    case 'POST':
-      let rawData = '';
-      req.on('data', (chunk) => {
-        rawData = rawData + chunk;
-      }).on('end', () => {
-        const decoded = decodeURIComponent(rawData);
-        console.info('[' + now + '] 投稿: ' + decoded);
-        res.write('<!DOCTYPE html><html lang="ja"><body><h1>' +
-          decoded + 'が投稿されました</h1></body></html>');
-        res.end();
-      });
-      break;
-    default:
-      break;
-  }
+  const server = http.createServer(basic, (req, res) => {
+  router.route(req, res);
 }).on('error', (e) => {
-  console.error('[' + new Date() + '] Server Error', e);
+  console.error('Server Error', e);
 }).on('clientError', (e) => {
-  console.error('[' + new Date() + '] Client Error', e);
+  console.error('Client Error', e);
 });
-const port = process.env.PORT || 8000;
+
+const port = 8000;
 server.listen(port, () => {
-  console.info('[' + new Date() + '] Listening on ' + port);
+  console.info('Listening on ' + port);
 });
